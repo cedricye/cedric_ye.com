a95bb9c60f49de67d7f963edf90252ff2b03da18
cedricye <yepf1212@gmail.com>
2013-11-18 19:32:46 +0800

    fix bugs

--- js/index.js
+++ js/index.js
@@ -1,52 +1,59 @@
 var ONE_SECOND = 1000;
-	var ONE_DAY = 60 * 60 * 24;
-	var timer;
+var ONE_DAY = 60 * 60 * 24;
+var timer;
 
-	function dayCountDown () {
-		var currentDate = new Date();
-		var deadDate = new Date('2073', '9', '15');
-		var leftSecond = parseInt((deadDate - currentDate) / ONE_SECOND);
-		var leftDay = parseInt(leftSecond / ONE_DAY);
+function dayCountDown () {
+	var currentDate = new Date();
+	var deadDate = new Date('2073', '9', '15');
+	var leftSecond = parseInt((deadDate - currentDate) / ONE_SECOND);
+	var leftDay = parseInt(leftSecond / ONE_DAY);
 
-		var leftDaySpan = document.getElementById("left_day");
-		var leftSecondSpan = document.getElementById("left_second");
+	var leftDaySpan = document.getElementById("left_day");
+	var leftSecondSpan = document.getElementById("left_second");
 
-		leftSecondSpan.innerHTML = leftSecond;
-		leftDaySpan.innerHTML = leftDay;
+	leftSecondSpan.innerHTML = leftSecond;
+	leftDaySpan.innerHTML = leftDay;
 
-	}
+}
 
-		//timer = setInterval(dayCountDown, 1000);
-		function init () {
-			var WIDTH = window.innerWidth,
-				HEIGHT = window.innerHeight,
-				groundpic = document.getElementById("groundpic"),
-				ground = document.getElementById("ground");
+window.onresize = function(){
+	init();
+	registerMouseMoveEvent();
+}
 
-			ground.style.width = WIDTH + "px";
-			ground.style.height = HEIGHT + "px";
+//timer = setInterval(dayCountDown, 1000);
+function init () {
+	var WIDTH = window.innerWidth < 850 ? 850 : window.innerWidth,
+		HEIGHT = window.innerHeight,
+		groundpic = document.getElementById("groundpic"),
+		ground = document.getElementById("ground");
 
-			groundpic.style.width = WIDTH + 410 + "px";
-			groundpic.style.left = -(140 / 1776) * (WIDTH + 410) + "px";
+	ground.style.width = WIDTH  + "px";
+	ground.style.height = HEIGHT + "px";
+
+	groundpic.style.width = WIDTH + 410 + "px";
+	groundpic.style.left = -(140 / 1776) * (WIDTH + 410) + "px";
+}
+
+function registerMouseMoveEvent () {
+	var WIDTH = window.innerWidth,			
+		groundpic = document.getElementById("groundpic"),
+		LEFT = parseInt(groundpic.style.left || groundpic.offsetLeft);
+
+
+	document.body.removeEventListener("mousemove");
+	document.body.addEventListener("mousemove", function (event) {
+		var currentX = event.clientX,
+			left;
+
+		if(currentX >= WIDTH / 2){
+			left = LEFT - (currentX - WIDTH / 2) * ((2 * 188) / WIDTH);
+		}else{
+			left = LEFT + (WIDTH / 2 - currentX) * ((2 * 140) / WIDTH);
 		}
 
-		function registerMouseMoveEvent () {
-			var WIDTH = window.innerWidth,			
-				groundpic = document.getElementById("groundpic"),
-				LEFT = parseInt(groundpic.style.left || groundpic.offsetLeft);
-
-			document.body.addEventListener("mousemove", function (event) {
-				var currentX = event.clientX,
-					left;
-
-				if(currentX >= WIDTH / 2){
-					left = LEFT - (currentX - WIDTH / 2) * ((2 * 188) / WIDTH);
-				}else{
-					left = LEFT + (WIDTH / 2 - currentX) * ((2 * 140) / WIDTH);
-				}
-
-				groundpic.style.left = left + "px";
-			})
-		}
-		init();
-		registerMouseMoveEvent();
+		groundpic.style.left = left + "px";
+	})
+}
+init();
+registerMouseMoveEvent();
